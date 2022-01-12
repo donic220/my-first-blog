3fc29edc7d68255f7555e41ff9aeaeca7709064f
Narushima Daiki <donic220@gmail.com>
2022-01-12 21:33:44 +0900

    不要箇所削除

--- mysite/settings.py
+++ mysite/settings.py
@@ -37,8 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'blog.apps.BlogConfig',
-    'widget_tweaks'
+    'blog.apps.BlogConfig'
 ]
 
 MIDDLEWARE = [
